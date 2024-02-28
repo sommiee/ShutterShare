c876c66c91cf637aa3eb7e828b77f5ef7cd60472
Sommie Ezenwa <76750174+sommiee@users.noreply.github.com>
2024-02-28 11:46:26 -0500

    connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 //Connection to Oracle
-$conn = oci_connect('sezenwa', '08208364', '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))');
+$conn = oci_connect('username', 'password', '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))');
 if (!$conn) {
     $m = oci_error();
     echo $m['message'];
